79718fe51f5b7cd3931eae2f9f65b1dbcd0e075e
blueneth <blueneth18@gmail.com>
2025-04-21 11:36:17 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '9207d96c1512b3cca9fd8206b4a4b662';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/w500';
     let currentItem;
